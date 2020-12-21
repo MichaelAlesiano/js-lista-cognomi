c6bd15d472dcfcba8da58b7635def80d75659be1
Michael <michael.alesiano@gmail.com>
2020-12-21 11:29:52 +0100

    Finished project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
-
 cognomi.push(prompt("Inserisci il tuo cognome"));
 
 for(var i = 1 ; i < cognomi.length ; i++){
        cognomi[i] = cognomi[i].charAt(0).toUpperCase() + cognomi[i].substr(1);
    }
 
+console.log(cognomi.sort());
 
-cognomi.sort();
 document.getElementById('lista_cognomi').innerHTML = cognomi;
-
-console.log(cognomi);
